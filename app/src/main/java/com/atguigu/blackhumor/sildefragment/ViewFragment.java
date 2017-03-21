3cd8c6a4ddfadea6c878c9c9f8fcf5c6bb343744
wly1995 <550612304@qq.com>
2017-03-21 16:40:04 +0800

    框架的完善

---
+++ app/src/main/java/com/atguigu/blackhumor/sildefragment/ViewFragment.java
@@ -0,0 +1,25 @@
+package com.atguigu.blackhumor.sildefragment;
+
+import com.atguigu.blackhumor.R;
+import com.atguigu.blackhumor.fragment.BaseFragment;
+
+/**
+ * Created by 万里洋 on 2017/3/21.
+ */
+
+public class ViewFragment extends BaseFragment {
+
+    @Override
+    protected void initListener() {
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+
+    @Override
+    public int getLayoutid() {
+        return R.layout.fragment_collection;
+    }
+}
---
+++ app/src/main/java/com/atguigu/blackhumor/sildefragment/WalletFragment.java
@@ -0,0 +1,25 @@
+package com.atguigu.blackhumor.sildefragment;
+
+import com.atguigu.blackhumor.R;
+import com.atguigu.blackhumor.fragment.BaseFragment;
+
+/**
+ * Created by 万里洋 on 2017/3/21.
+ */
+
+public class WalletFragment extends BaseFragment {
+
+    @Override
+    protected void initListener() {
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+
+    @Override
+    public int getLayoutid() {
+        return R.layout.fragment_wallet;
+    }
+}
